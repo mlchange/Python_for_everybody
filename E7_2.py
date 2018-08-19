957f25ea57c38bc018b3eee59ca1105f524614a4
Maverick <lightjade88@gmail.com>
2018-08-19 12:54:05 +0800

    add e7_2

---
+++ E7_2.py
@@ -0,0 +1,16 @@
+# Use the file name mbox-short.txt as the file name
+
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+confident=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : 
+        continue
+    count=count+1
+    n=line.find(':')
+    confident=confident+float(line[n+1:])
+
+print("Average spam confidence:",confident/count)
+
+print('ad')
